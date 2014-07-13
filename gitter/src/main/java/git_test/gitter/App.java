64cf35572e53d3a186bd768b8cd7a8d25c90575d
DariuszLaciak <dariusz.laciak@gmail.com>
2014-07-13 13:36:54 +0200

    test

---
+++ gitter/src/main/java/git_test/gitter/App.java
@@ -0,0 +1,13 @@
+package git_test.gitter;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
